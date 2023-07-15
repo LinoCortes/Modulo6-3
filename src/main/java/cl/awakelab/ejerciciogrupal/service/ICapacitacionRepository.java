d8c00e643fcc26a8e48bbe8702193a33a86b63c0
Galtor-program <felipetorog@gmail.com>
2023-07-14 20:08:43 -0400

    Se soluciona problema de perfiles

--- src/main/java/cl/awakelab/ejerciciogrupal/service/ICapacitacionRepository.java
+++ src/main/java/cl/awakelab/ejerciciogrupal/service/ICapacitacionRepository.java
@@ -1,7 +1,7 @@
 package cl.awakelab.ejerciciogrupal.service;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import cl.awakelab.ejerciciogrupal.modelo.Capacitacion;
 
